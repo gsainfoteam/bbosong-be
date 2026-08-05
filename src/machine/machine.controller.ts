05f5f47eb104b8d0459c083dc1f2859d2bac75c6
jjwopen <jjwjjw2007@gmail.com>
2026-08-05 13:27:32 +0900

    Add Admin role(LDB-9)

--- src/machine/machine.controller.ts
+++ src/machine/machine.controller.ts
@@ -19,7 +19,8 @@ import {
   ApiOkResponse,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { AdminGuard, UserGuard } from '../auth/guard/user.guard';
+import { UserGuard } from '../auth/guard/user.guard';
+import { AdminGuard } from '../auth/guard/admin.guard';
 import { MachinePowerApiKeyGuard } from '../auth/guard/machine-power-api-key.guard';
 import { GetUser } from '../auth/decorator/get-user.decorator';
 import { User } from 'generated/prisma/client';
