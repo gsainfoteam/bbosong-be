7843e8df677cf7d9e4cb3776ac4bb2ef7ef73a8e
jjwopen <jjwjjw2007@gmail.com>
2026-08-26 15:43:06 +0900

    Edit DTO (#38)

--- src/machine/dto/req/create-power-req.dto.ts
+++ src/machine/dto/req/create-power-req.dto.ts
@@ -1,7 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDefined, IsNotEmpty, IsNumber, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class CreatePowerReqDto {
+  @ApiProperty({
+    description: 'Power value to record',
+    example: 1200,
+    minimum: 0,
+  })
   @IsDefined()
   @IsNotEmpty()
   @Transform(({ value }) =>
