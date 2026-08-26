bd47966641d30301ba32ac235e235cc5e8303e1b
jjwopen <jjwjjw2007@gmail.com>
2026-08-26 15:42:32 +0900

    Edit DTO

--- src/notification/dto/req/subscribe-req.dto.ts
+++ src/notification/dto/req/subscribe-req.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsString,
   IsObject,
@@ -7,20 +8,37 @@ import {
 import { Type } from 'class-transformer';
 
 class PushSubscriptionKeysDto {
+  @ApiProperty({
+    description: 'Public key used to encrypt push messages (P-256 ECDH)',
+    example:
+      'BIPUL12DLfytvTajnryr2PRdAgXS3HGKiLqndGcJGabyhIfLfnFVwtiRlnqYD03qMzPTgy2M...',
+  })
   @IsString()
   @IsNotEmpty()
   p256dh: string;
 
+  @ApiProperty({
+    description: 'Authentication secret used to encrypt push messages',
+    example: 'FPssNDTKnInHVndSTdbKFw',
+  })
   @IsString()
   @IsNotEmpty()
   auth: string;
 }
 
 export class SubscribeReqDto {
+  @ApiProperty({
+    description: 'Push service endpoint URL for the subscribed client',
+    example: 'https://fcm.googleapis.com/fcm/send/dGVzdC1lbmRwb2ludC1leGFtcGxl',
+  })
   @IsString()
   @IsNotEmpty()
   endpoint: string;
 
+  @ApiProperty({
+    description: 'Encryption keys for the push subscription',
+    type: PushSubscriptionKeysDto,
+  })
   @IsObject()
   @ValidateNested()
   @Type(() => PushSubscriptionKeysDto)
