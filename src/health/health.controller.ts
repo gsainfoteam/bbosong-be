eeccadf752119f14896adb556dd6cb2ff23f2272
Boseong <lee@paperst.ar>
2026-09-20 22:32:35 +0900

    feat: add health module (#54)

---
+++ src/health/health.controller.ts
@@ -0,0 +1,33 @@
+import { DatabaseService } from '@lib/database';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  HealthCheck,
+  HealthCheckService,
+  MemoryHealthIndicator,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+
+// Controller for health check
+@ApiTags('health')
+@Controller('health')
+export class HealthController {
+  constructor(
+    private readonly prismaService: DatabaseService,
+    private readonly health: HealthCheckService,
+    private readonly prisma: PrismaHealthIndicator,
+    private readonly memory: MemoryHealthIndicator,
+  ) {}
+
+  @ApiOperation({
+    summary: 'Health check for the application',
+    description: 'Check the health of the application',
+  })
+  @Get()
+  @HealthCheck()
+  async check() {
+    return await this.health.check([
+      () => this.prisma.pingCheck('database', this.prismaService),
+    ]);
+  }
+}
