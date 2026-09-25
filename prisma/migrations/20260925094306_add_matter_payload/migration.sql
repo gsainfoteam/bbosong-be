23fb1330766d812ab02e2ac829c8c6628e59c0c2
2paperstar <lee@paperst.ar>
2026-09-25 19:13:06 +0900

    feat: provide matter payload in create machine dto

--- prisma/migrations/20260925094306_add_matter_payload/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "machine" ADD COLUMN     "is_commissioned" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "mac_address" TEXT,
-ADD COLUMN     "matter_payload" TEXT NOT NULL DEFAULT '';
--- manual backfill needed for matter_payload column
