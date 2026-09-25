4008e02f75738beb1ca9333be98d3927992ad14d
2paperstar <lee@paperst.ar>
2026-09-25 18:59:24 +0900

    fix: make matter payload empty for backfill

--- prisma/migrations/20260925094306_add_matter_payload/migration.sql
+++ prisma/migrations/20260925094306_add_matter_payload/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-DELETE FROM "machine";
 ALTER TABLE "machine" ADD COLUMN     "is_commissioned" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "mac_address" TEXT,
-ADD COLUMN     "matter_payload" TEXT NOT NULL;
+ADD COLUMN     "matter_payload" TEXT NOT NULL DEFAULT '';
+-- manual backfill needed for matter_payload column
