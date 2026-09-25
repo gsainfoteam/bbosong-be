7778aeb0b9c82beb92601bafa4d2132ce1908351
2paperstar <lee@paperst.ar>
2026-09-26 07:51:14 +0900

    fix: url parts missing after first `=` token

--- src/machine/machine.module.ts
+++ src/machine/machine.module.ts
@@ -19,7 +19,8 @@ import { MachineService } from './machine.service';
           .getOrThrow<string>('MATTER_SITES')
           .split(',')
           .map((config) => {
-            const [id, wsUrl] = config.split('=');
+            const [id, ...urlParts] = config.split('=');
+            const wsUrl = urlParts.join('=');
             return { id, wsUrl };
           }),
       }),
